7a81bddb6aa3e5a6f2295f383a87f3643341ef13
Dirk Lorenz <dirk.lorenz2404@gmail.com>
2019-10-20 15:25:07 +0200

    eslindt

--- mail.js
+++ mail.js
@@ -1,22 +1,22 @@
+/* eslint-disable no-console */
 const fgetSunRS = function getSunRS() {
-  let SunRS = {
+  const SunRS = {
     SunRise: 'init',
-    SunSet: 'init'
+    SunSet: 'init',
   };
 
-
-
+  // eslint-disable-next-line no-undef
   http.get('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today', (res) => {
     const { statusCode } = res;
     const contentType = res.headers['content-type'];
 
     let error;
     if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' +
-        `Status Code: ${statusCode}`);
+      error = new Error('Request Failed.\n'
+        + `Status Code: ${statusCode}`);
     } else if (!/^application\/json/.test(contentType)) {
-      error = new Error('Invalid content-type.\n' +
-        `Expected application/json but received ${contentType}`);
+      error = new Error('Invalid content-type.\n'
+        + `Expected application/json but received ${contentType}`);
     }
     if (error) {
       console.error(error.message);
@@ -43,7 +43,5 @@ const fgetSunRS = function getSunRS() {
   });
 
   return SunRS;
-
-}
-export default fgetSunRS;
-
+};
+module.export = fgetSunRS;
